7e0c924fa1d657ccd00b6c75ce57aa56d9366f8a
Amrita Mazumdar <amritamaz@gmail.com>
2014-04-06 14:20:48 -0400

    more to lexer

--- lexer.py
+++ lexer.py
@@ -1,20 +1,53 @@
 
-tokens = (
-    'NAME','NUMBER',
+reserved = {
+  'for'   : 'FOR',
+  'while' : 'WHILE',
+  'if'    : 'IF',
+  'else'  : 'ELSE',
+  'return': 'RETURN',
+  'in'    : 'IN',
+  'true'  : 'TRUE',
+  'false' : 'FALSE',
+  'and'   : 'AND',
+  'or'    : 'OR',
+  'not'   : 'NOT',
+}
+
+tokens = [
+    'ID','NUMBER',
     'PLUS','MINUS','TIMES','DIVIDE','EQUALS',
-    'LPAREN','RPAREN',
-    )
+    'LPAREN','RPAREN','COMMENT','LBRACK','RBRACK',
+    'COLON','COMMA', 'HASH','AT','LESSTHANEQ',
+    'GREATERTHANEQ', 'LESSTHAN', 'GREATERTHAN', 'NEQUAL',
+    'STRING','NEWLINE','INDENT','DEDENT',
+    ] + list(reserved.values())
 
 # Tokens
 
-t_PLUS    = r'\+'
-t_MINUS   = r'-'
-t_TIMES   = r'\*'
-t_DIVIDE  = r'/'
-t_EQUALS  = r'='
-t_LPAREN  = r'\('
-t_RPAREN  = r'\)'
-t_NAME    = r'[a-zA-Z_][a-zA-Z0-9_]*'
+t_PLUS        = r'\+'
+t_MINUS       = r'-'
+t_TIMES       = r'\*'
+t_COMMENT     = r'//'
+t_DIVIDE      = r'/'
+t_EQUALS      = r'='
+t_LPAREN      = r'\('
+t_RPAREN      = r'\)'
+t_LBRACK      = r'\['
+t_RBRACK      = r'\]'
+t_COLON       = r':'
+t_COMMA       = r','
+t_STRING      = r'\"([^"])*\"'
+t_HASH        = r'#'
+t_AT          = r'@'
+t_LESSTHANEQ  = r'<='
+t_GREATERTHANEQ = r'>='
+t_LESSTHAN    = r'<'
+t_GREATERTHAN = r'>'
+t_NEQUAL      = r'!='
+t_NEWLINE     = r'\n'
+t_INDENT      = r'\t'
+t_ID    = r'[a-zA-Z_][a-zA-Z0-9_]*'
+
 
 def t_NUMBER(t):
     r'\d+'
@@ -26,12 +59,8 @@ def t_NUMBER(t):
     return t
 
 # Ignored characters
-t_ignore = " \t"
+#t_ignore = " \t"
 
-def t_newline(t):
-    r'\n+'
-    t.lexer.lineno += t.value.count("\n")
-    
 def t_error(t):
     print("Illegal character '%s'" % t.value[0])
     t.lexer.skip(1)
