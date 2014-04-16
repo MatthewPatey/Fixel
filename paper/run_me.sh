46997e098f167c21fb62b2205851890f271fdcc2
Amrita Mazumdar <amritamaz@gmail.com>
2014-04-16 17:44:35 -0400

    script now compiles md to tex and all tex to pdf together

--- paper/run_me.sh
+++ paper/run_me.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
+echo "Converting Markdown to TeX..."
+SECTIONS="./sections/*.md"
+
+for f in $SECTIONS
+do
+    echo "Processing $f"
+    pandoc $f -o ${f%.md}.tex
+done
+
 echo "Producing PDF..."
-pdflatex interaction=batchmode paper.tex
+pdflatex paper.tex
+pdflatex paper.tex
