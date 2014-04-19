b3063e57c2e0045f6b5f19c0fe48c17d046ff5e3
Matthew Patey <mep2167@columbia.edu>
2014-04-18 22:55:27 -0400

    Added custom functions for nodes to generator. This allows function expressions to generate empty '(' ')' if it has no parameters.

--- code/src/generator.py
+++ code/src/generator.py
@@ -1,10 +1,3 @@
-lookup_table = {
-    'parameters': ('(', ')'),
-    'expression_statement': (None, '\n')
-}
-
-
-ignore = ['#', '@', '']
 
 
 class Generator:
@@ -19,21 +12,53 @@ class Generator:
     def process_tree(self, node):
         if len(node.children) > 0:
             # if it's not a leaf, process it
-            # first check if there are preprocess strings (paren, newline, etc)
-            if node.value in lookup_table.keys():
-                current_tuple = lookup_table[node.value]
+            # first check if there are preprocess strings (paren, cnewline, etc)
+            if node.value in pre_post_table.keys():
+                current_tuple = pre_post_table[node.value]
             else:
                 current_tuple = (None, None)
-            # add pre/post processing info
+            # add pre processing info
             if current_tuple[0] is not None:
                 self.string_list.append(current_tuple[0])
 
             # process the children
             for child in node.children:
                 self.process_tree(child)
-            # add info for the current node to the list
+
+            # add post processing info
             if current_tuple[1] is not None:
                 self.string_list.append(current_tuple[1])
+
+            # execute custom behavior before returning
+            if node.value in custom_function_table.keys():
+                custom_function = custom_function_table[node.value]
+                custom_function(self, node)
         elif node.value not in ignore:
             # if it's a leaf add it to the string list
             self.string_list.append(node.value)
+
+    def function_expression_custom_function(self, node):
+        """
+        handles case of no parameters in a function expression
+        """
+        if len(node.children[2].value) == 0:
+            self.string_list.append('()')
+
+
+"""
+keys are node names (grammar sybmols) and
+values are tuple of strings to print before and after the children are processes
+"""
+pre_post_table = {
+    'parameters': ('(', ')'),
+    'expression_statement': (None, '\n')
+}
+
+
+# keys are node names and values are functions that allow custom behavior for a given node type
+custom_function_table = {
+    'function_expression': Generator.function_expression_custom_function
+}
+
+
+ignore = ['#', '@', '']
