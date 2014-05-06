233a28db5cf38ff6fe1c5a50ca33e39414583d3c
Matthew Patey <mep2167@columbia.edu>
2014-05-06 17:02:52 -0400

    run translator changes working directory during translation so that parsing output files always go to the same place. changes back to original before writing out files.

--- code/run_translator.py
+++ code/run_translator.py
@@ -1,4 +1,5 @@
 import argparse
+import os
 from src import translator
 
 arg_parser = argparse.ArgumentParser()
@@ -6,11 +7,19 @@ arg_parser.add_argument('-v', '--verbose', help='prints every intermediate step 
 						action='store_true', default=False)
 arg_parser.add_argument('-f', '--fixel-file', type=argparse.FileType('r'))
 namespace = arg_parser.parse_args()
+
 if namespace.fixel_file:
 	source_string = namespace.fixel_file.read()
 	file_name = namespace.fixel_file.name
 	namespace.fixel_file.close()
+
+	pwd = os.getcwd()
+	script_dir = os.path.dirname(__file__)  # directory this script is in
+	os.chdir(script_dir)  # change directory so parse outputs are in right place
+
 	result = translator.translate(source_string, namespace.verbose)
+
+	os.chdir(pwd)  # go back to original pwd before writing out file
 	f = open(file_name.split('.')[0] + '.py', 'w')
 	f.write(result)
 	f.close()
