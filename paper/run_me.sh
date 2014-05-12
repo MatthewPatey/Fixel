841961289f2be1ac99b4263be11c1b3821c9d20e
Amrita Mazumdar <amritamaz@gmail.com>
2014-05-11 20:22:23 -0400

    finally got git commit history auto-adding into the paper nicely

--- paper/run_me.sh
+++ paper/run_me.sh
@@ -1,5 +1,29 @@
 #!/bin/bash
 
+echo "Generating Git commit log..."
+git log --pretty=format:"%an & %ad & %s
+" --date=short > gitlog1.md
+echo "\begin{center}
+\begin{longtabu} to \textwidth {|
+    X[4,l]|
+    X[3,c]|
+    X[8,l]|}
+    \hline
+    \textbf{Author} & \textbf{Date} & \textbf{Message} \\ \hline" > gitlog.tex
+
+awk '{printf "|%s\n",$0}' < gitlog1.md >> gitlog2.md
+pandoc gitlog2.md -o gitlog1.tex
+tr '\n' ' ' < gitlog1.tex > gitlog2.tex
+sed -i -e 's/\\textbar{}/\'$'\n/g' gitlog2.tex
+sed '/^ $/d' gitlog2.tex > gitlog3.tex
+sed '/^$/d' gitlog3.tex > gitlog2.tex
+awk '{printf "%s\\\\ \\hline\n",$0}' < gitlog2.tex >> gitlog2
+sed -i -e 's/\\&/\&/g' gitlog2
+cat gitlog2 >> gitlog.tex
+echo "\end{longtabu}
+\end{center}" >> gitlog.tex
+rm gitlog1* gitlog2* gitlog3*
+
 echo "Converting Markdown to TeX..."
 SECTIONS="./sections/*.md"
 
